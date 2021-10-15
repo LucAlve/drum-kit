79ad607c79539ee852b986a2688edddef585db44
LucAlve <71749837+LucAlve@users.noreply.github.com>
2021-10-14 23:14:25 -0500

    javascript commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+document.getElementsByClassName("w")[0].addEventListener("click", function() {
+  alert("got clicked");
+});
+
+
+document.getElementsByClassName("a")[0].addEventListener("click", function() {
+  alert("got clicked");
+});
+
+document.getElementsByClassName("s")[0].addEventListener("click", function() {
+  alert("got clicked");
+});
+
+document.getElementsByClassName("d")[0].addEventListener("click", function() {
+  alert("got clicked");
+});
+
+document.getElementsByClassName("j")[0].addEventListener("click", function() {
+  alert("got clicked");
+});
+
+document.getElementsByClassName("k")[0].addEventListener("click", function() {
+  alert("got clicked");
+});
+
+document.getElementsByClassName("k")[0].addEventListener("click", function() {
+  alert("got clicked");
+});
