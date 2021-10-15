99d28524778d192f8292ef6618d367c053219295
LucAlve <71749837+LucAlve@users.noreply.github.com>
2021-10-15 17:36:51 -0500

    Final commit

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
 
 for (i=0; i<document.querySelectorAll(".drum").length; i++) {
   document.getElementsByClassName("drum")[i].addEventListener("click", function() {
-    alert("got clicked " + i);
+    
+    var buttonInnerHTML = this.innerHTML
+
+    switch (buttonInnerHTML) {
+      case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+    
+      case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+      case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+
+      case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+      case "j":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+
+      case "k":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+
+      case "l":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+
+        default: console.log(buttonInnerHTML)
+
+    }   
+
+    
   });
 }
-
-//
-// document.getElementsByClassName("a")[0].addEventListener("click", function() {
-//   alert("got clicked");
-// });
-//
-// document.getElementsByClassName("s")[0].addEventListener("click", function() {
-//   alert("got clicked");
-// });
-//
-// document.getElementsByClassName("d")[0].addEventListener("click", function() {
-//   alert("got clicked");
-// });
-//
-// document.getElementsByClassName("j")[0].addEventListener("click", function() {
-//   alert("got clicked");
-// });
-//
-// document.getElementsByClassName("k")[0].addEventListener("click", function() {
-//   alert("got clicked");
-// });
-//
-// document.getElementsByClassName("l")[0].addEventListener("click", function() {
-//   alert("got yelled");
-// });
